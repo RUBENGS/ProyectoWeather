3feb700a962e4d2f9aa12e5181f661142efa8ab1
RUBENGS <rubengarciasalas@gmail.com>
2021-10-19 09:57:21 +0200

    V2 geolocation

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@ function Weather() {
   this.apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast";
 }
 
+Weather.prototype.getWeatherHere = function () {
+  var that = this;
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+};
+
 Weather.prototype.getWeather = function (nombreCiudad) {
   var that = this;
 
@@ -150,6 +155,7 @@ $(document).ready(function () {
   $("#informacion").hide();
   $("#weather").hide();
   $("#forecast").hide();
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
   var weatherClient = new Weather();
 
@@ -160,6 +166,11 @@ $(document).ready(function () {
 
   $("#buscarAhora").submit(function (event) {
     weatherClient.getWeather($("input").eq(1).val());
+    event.preventDefault();
+  });
+
+  $("#weatherHere").click(function (event) {
+    weatherClient.getWeatherHere();
     event.preventDefault();
   });
 
@@ -183,7 +194,71 @@ $(document).ready(function () {
     $("#presentacion").hide();
     $("#informacion").hide();
     $("#weather").hide();
-    $("#forecast").fadeIn();
+    $("#forecast").hide();
     event.preventDefault();
   });
 });
+
+var onSuccess = function (position) {
+  $.get(
+    "https://api.openweathermap.org/data/2.5/weather" +
+      "?lat=" +
+      position.coords.latitude +
+      "&lon=" +
+      position.coords.longitude +
+      "&appid=8f39cd935257c74fd2f95fdb1da6ba35",
+    function (json) {
+      var result = "";
+
+      if (json && json.cod == 200) {
+        result = "<p class='title'>Tiempo en " + json.name + "</p>";
+        switch (json.weather[0].main) {
+          case "Clear":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/01d@4x.png">';
+            break;
+          case "Clouds":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/02d@4x.png">';
+            break;
+          case "Rain":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/10d@4x.png">';
+            break;
+          case "Thunderstorm":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/11d@4x.png">';
+            break;
+          case "Snow":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/13d@4x.png">';
+            break;
+          case "Mist":
+            result +=
+              '<img src="https://openweathermap.org/img/wn/50d@4x.png">';
+            break;
+          default:
+            result += "";
+        }
+        result += "<p>" + json.weather[0].main + "</p>";
+        result += "<p>" + (json.main.temp - 273.15).toFixed(2) + "ºC</p>";
+      }
+
+      $("#informacion").html(result);
+      $("#informacion").fadeIn();
+      $("#presentacion").hide();
+    }
+  ).fail(function () {
+    var result = "";
+    result = "<p>Error</p>";
+    $("#informacion").html(result);
+    $("#presentacion").hide();
+    $("#informacion").fadeIn();
+  });
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+  alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
+}
